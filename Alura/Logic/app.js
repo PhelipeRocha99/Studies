9c64dbfd94e4615ac8b3bdd7420ccff46e603c37
Phelipe Rocha B. Conceição <phelipedjk@gmail.com>
2025-09-09 05:18:34 -0300

    Alura's Programming logic: Javascript -  Class 7 - Functions

--- Alura/Logic/app.js
+++ Alura/Logic/app.js
@@ -1,11 +1,20 @@
-//JS- document.querySelector get all from html tag specific in parenthesis
-let title = document.querySelector('h1');
-title.innerHTML = "Secret Number Game";
+let secretNumber = randomNumberGenerator();
+function printTextOnScreen(tag,text){
+    //JS- document.querySelector get all from html tag specific in parenthesis
+    let field = document.querySelector(tag);
+    field.innerHTML = text;
+}
 
-let paragraph = document.querySelector('p');
-paragraph.innerHTML = "Choose a number between 1 and 10";
+printTextOnScreen("h1", "Secret Number Game");
+printTextOnScreen("p","Choose a number between 1 and 100");
+
 
 //programming logic - Function must have only one responsability.
 function verifyGuess(){
+    let guess = document.querySelector("input").value;
+    console.log(guess == secretNumber);
+}
 
+function randomNumberGenerator() {
+    return parseInt(Math.random() * 10 + 1);
 }
