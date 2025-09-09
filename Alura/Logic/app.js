df7b73d63ee3d96c72a63ccca6aa99fe8dd255d8
Phelipe Rocha B. Conceição <phelipedjk@gmail.com>
2025-09-09 04:58:24 -0300

    Alura's Programming logic: Javascript -  Class 6 - Interaction with HTML and Functions

---
+++ Alura/Logic/app.js
@@ -0,0 +1,11 @@
+//JS- document.querySelector get all from html tag specific in parenthesis
+let title = document.querySelector('h1');
+title.innerHTML = "Secret Number Game";
+
+let paragraph = document.querySelector('p');
+paragraph.innerHTML = "Choose a number between 1 and 10";
+
+//programming logic - Function must have only one responsability.
+function verifyGuess(){
+
+}
